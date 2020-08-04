c956e70b005f1f20220a295ff967508d4c370ca2
GSLevel <67515314+GSLevel@users.noreply.github.com>
2020-08-04 10:31:58 +0100

    index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var lvl = document.getElementById('lvl');
 var moreBtn = document.getElementById('moreBtn');
+var clickBtn = document.getElementById('clickBtn');
 
 moreBtn.onclick = function() {
 
@@ -12,3 +13,38 @@ moreBtn.onclick = function() {
     }
 
 };
+
+function change() {
+    clickBtn.innerHTML = "sub to gs level for good content starting in september"
+}
+
+function play() {
+    var audio = document.getElementById('clickme');
+    audio.play();
+}
+
+function play1() {
+    var audio = document.getElementById('showmore');
+    audio.play();
+}
+
+function play2() {
+    var audio = document.getElementById('portfolio');
+    audio.play();
+}
+
+function play3() {
+    var audio = document.getElementById('blocc');
+    audio.play();
+}
+
+function play4() {
+    var audio = document.getElementById('srchh');
+    audio.play();
+}
+
+function play5() {
+    var audio = document.getElementById('sub');
+    audio.play();
+}
+
